8d36cc3ac66bdd4bc3caa3c39efc707c3c3385d3
Honza Cibulka <honza@datastory.cz>
2020-09-24 18:34:18 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+﻿import "./byeie"; // loučíme se s IE
+
+/*
+// snadné načtení souboru pro každého!
+fetch("https://blabla.cz/blabla.json")
+  .then(response => response.json()) // nebo .text(), když to není json
+  .then(data => {
+    // tady jde provést s daty cokoliv
+  });
+*/
